834a0f8559c89ef989eef5e89a33daf3d9cdead6
Adão Lima <adaoolima@gmail.com>
2021-01-23 15:40:41 -0300

    Added Helmet

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 
-const Cart:React.FC = () => ( <h1>Cart</h1> );
+const Cart:React.FC = () => ( 
+    <>
+        <Helmet>
+            <title> Your Cart </title>
+            <meta name="description" content="These amazing products area almost yours." />
+        </Helmet>
+        <h1>Cart</h1> 
+    </>
+);
 
 export default Cart;
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
-const Catalog:React.FC = () => ( <h1>Catalog</h1> );
+const Catalog:React.FC = () => ( 
+    <>
+        <Helmet>
+            <title>Catalog</title>
+            <meta name="description" content="Many amazing products." />
+        </Helmet>
+        <h1>Catalog</h1> 
+    </>
+);
 
 export default Catalog;
