2cc80b44445dff3232e2dd538f4e3bf91d3421b1
Adão Lima <adaoolima@gmail.com>
2021-01-24 11:57:46 -0300

    Scaffolding Catalog page

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { ICart } from '../../store/atoms';
 
-const Catalog:React.FC = () => ( 
-    <>
-        <Helmet>
-            <title>Catalog</title>
-            <meta name="description" content="Many amazing products." />
-        </Helmet>
-        <h1>Catalog</h1> 
-    </>
-);
+const coursesId = [
+    "How to Learn",
+    "Javascript",
+    "ReactJs",
+    "NextJs",
+    "RecoilJs",
+    "ReduxJs",
+    "VueJs",
+    "NuxtJs",
+    "Vuex",
+];
+
+const courses: ICart[] = coursesId.map((id, index) => ({ id, price: index + 1 }));
+
+const Catalog:React.FC = () => {
+    const addItem = () => {};
+    return ( 
+        <>
+            <Helmet>
+                <title>Courses catalog</title>
+                <meta name="description" content="Amazing courses to improve your skills." />
+            </Helmet>
+            <div className="catalog">
+                <div className="courses">
+                    {courses.map((course) => (
+                        <div key={course.id} className="course">
+                            <h4>
+                                {course.id} / ${course.price}
+                            </h4>
+                            <button
+                                className="button"
+                                onClick={() => {}}
+                            >
+                                Add
+                            </button>
+                        </div>  
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+};
 
 export default Catalog;
