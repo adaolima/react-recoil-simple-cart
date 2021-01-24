7446823372ba48fc38ad63f0e1618006c798fbee
Adão Lima <adaoolima@gmail.com>
2021-01-24 11:57:30 -0300

    Card Buutons Scaffolding

--- src/components/CartButtons/index.tsx
+++ src/components/CartButtons/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const CartButtons:React.FC = () => ( <button> Button </button> );
+
+const CartButtons:React.FC = ({ item }) => {
+    const add = () => {};
+    const remove = () => {};
+    const decrease = () => {};
+
+    return ( 
+        <div className="group-buttons">
+            <button onClick={() => decrease(item)} title="Decrease"> - </button> 
+            <button onClick={() => add(item)} title="Increase"> + </button> 
+            <button onClick={() => remove(item)} title="Remove"> x </button> 
+        </div>
+    )
+
+};
 
 export default CartButtons;
